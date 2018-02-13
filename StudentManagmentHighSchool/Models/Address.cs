289397887f2b6ff6242a2c8a004bce12edb9222b
addisalemtafere <addisalem12@gmail.com>
2018-02-13 22:38:41 +0300

    address model added

---
+++ StudentManagmentHighSchool/Models/Address.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentManagmentHighSchool.Models
+{
+    public class Address
+    {
+        public int AddressId { get; set; }
+        public string Region { get; set; }
+        public string Ciity { get; set; }
+        public string kebele { get; set; }
+        public string Email { get; set; }
+        public int Telephone { get; set; }
+        public Staff Staff { get; set; }
+        public Student Student { get; set; }
+    }
+}
